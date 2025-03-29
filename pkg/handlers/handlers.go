2ef541721d80236ef4a44901d29b8ad9c9a91fdf
agral <adam@gralin.ski>
2025-03-29 09:25:45 +0100

    Extract rendering of templates to custom pkg

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,24 +1,14 @@
 package handlers
 
 import (
-	"fmt"
+	"Invoicer/pkg/render"
 	"net/http"
-	"text/template"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Printf("Error parsing template %q: %s\n", tmpl, err)
-		return
-	}
-}
-
 func Home(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "home.tmpl")
+	render.RenderTemplate(w, "home.tmpl")
 }
 
 func Status(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "status.tmpl")
+	render.RenderTemplate(w, "status.tmpl")
 }
